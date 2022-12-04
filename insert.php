b93e34814837e959fe88f18fefcc916950f605c9
shadowgamer1216 <meamill88@gmail.com>
2022-12-03 21:17:02 -0800

    Final commit of files
    Final product for Deliverable 3

--- insert.php
+++ insert.php
@@ -9,23 +9,17 @@ $connect = new mysqli($servername, $username, $password, $database);
 if ($connect->connect_error) {
 	die("Connection failed: " . $connect->connect_error);
 }
-echo "Connected successfully <br>";
-echo "Inserting data... [Started] <br>";
 
 #Insert
 $expected_userid = $_POST["userID"];
 $statement = $connect->prepare("INSERT INTO WebComicUser (userID) VALUES (?)");
 $statement->bind_param("s", $_POST['userID']);
-#$statement->execute();
-echo "Inserting data... [Finished]<br>";
+$statement->execute();
 
 #Select
-echo "Showing all data... [Started]<br>";
 $select = "SELECT * FROM WebComicUser";
 $result = $connect->query($select);
-#echo "<table>";
-#echo "<table border='1'>
-echo "<table>
+echo "<table border='1'>
 <tr>
 <th>userID</th>
 <th>username</th>
@@ -34,21 +28,15 @@ echo "<table>
 <th>isReader</th>
 </tr>";
 while ($row = $result->fetch_assoc()) {
-#	echo "<tr>";
-	#echo "userID: " . $row["userID"];
-	#echo "<td>" . $row["userID"] "</td>"; 
-	#echo "username: " . $row["username"] 
-#	echo "<td>" . $row["username"] "</td>";
-	#echo "join_date: " . $row["join_date"] 
-#	echo "<td>" . $row["join_date"] "</td>";
-	#echo "isCreator: " . $row["isCreator"] 
-#	echo "<td>" . $row["isCreator"] "</td>";
-	#echo "isReader: " . $row["isReader"];
-#	echo "<td>" . $row["isReader"] "</td";
-#	echo "</tr>";
+	echo "<tr>";
+	echo "<td>" . $row["userID"] . "</td>"; 
+	echo "<td>" . $row["username"] . "</td>";
+	echo "<td>" . $row["join_date"] . "</td>";
+	echo "<td>" . $row["isCreator"] . "</td>";
+	echo "<td>" . $row["isReader"] . "</td";
+	echo "</tr>";
 }
 echo "</table>";
-echo "Showing all data... [Finished]<br>";
 $statement->close();
 $connect->close()
 ?>
