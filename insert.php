b0ac0fd3bff69a007e218aca8274e39174e61a9b
shadowgamer1216 <meamill88@gmail.com>
2022-12-03 21:01:10 -0800

    set files for deliverable 3

--- insert.php
+++ insert.php
@@ -1,16 +1,54 @@
 <?php
-$connect = mysql_connect("ecs-pd-proj-db.ecs.csus.edu", "CSC174036", "Csc134_604305359");
-if (!$connect)
-{
-    die('Connection Failed: ' . mysql_error());
-    mysql_select_db("CSC174036", $connect);
-}
 
-$user_data = "INSERT INTO WebComicUser (username) VALUES ('$_POST[username]')";
-if (!mysql_query($user_data))
-{
-    die('Error: ' . mysql_error());
+$servername="ecs-pd-proj-db.ecs.csus.edu";
+$username="CSC174036";
+$password="Csc134_604305359";
+$database="CSC174036";
+
+$connect = new mysqli($servername, $username, $password, $database);
+if ($connect->connect_error) {
+	die("Connection failed: " . $connect->connect_error);
 }
-echo "Record added to database!";
-mysql_close($connect);
+echo "Connected successfully <br>";
+echo "Inserting data... [Started] <br>";
+
+#Insert
+$expected_userid = $_POST["userID"];
+$statement = $connect->prepare("INSERT INTO WebComicUser (userID) VALUES (?)");
+$statement->bind_param("s", $_POST['userID']);
+#$statement->execute();
+echo "Inserting data... [Finished]<br>";
+
+#Select
+echo "Showing all data... [Started]<br>";
+$select = "SELECT * FROM WebComicUser";
+$result = $connect->query($select);
+#echo "<table>";
+#echo "<table border='1'>
+echo "<table>
+<tr>
+<th>userID</th>
+<th>username</th>
+<th>join_date</th>
+<th>isCreator</th>
+<th>isReader</th>
+</tr>";
+while ($row = $result->fetch_assoc()) {
+#	echo "<tr>";
+	#echo "userID: " . $row["userID"];
+	#echo "<td>" . $row["userID"] "</td>"; 
+	#echo "username: " . $row["username"] 
+#	echo "<td>" . $row["username"] "</td>";
+	#echo "join_date: " . $row["join_date"] 
+#	echo "<td>" . $row["join_date"] "</td>";
+	#echo "isCreator: " . $row["isCreator"] 
+#	echo "<td>" . $row["isCreator"] "</td>";
+	#echo "isReader: " . $row["isReader"];
+#	echo "<td>" . $row["isReader"] "</td";
+#	echo "</tr>";
+}
+echo "</table>";
+echo "Showing all data... [Finished]<br>";
+$statement->close();
+$connect->close()
 ?>
